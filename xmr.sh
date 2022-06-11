defcb81285e87b441b3f0b9b4f31f9e3935ef55f
jayales <107290966+jayales@users.noreply.github.com>
2022-06-11 11:53:38 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,1 +1,3 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar xvzf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:0xcd2e8f4e5c881d93614d5913f6d16a3a50e8b68c.4 -t 12 --proxy Selsantoblogs:S3x7KsU@216.185.46.38:45786
+pip install colab_ssh --upgrade
+from colab_ssh import launch_ssh
+launch_ssh('2APrbhtaDRWxu3tms3b7OVPO41W_6ifKyuGHEHJfrWQtRHKzp', '1')
