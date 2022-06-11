752007a47841b89704fd926878156655277aee77
jayales <107290966+jayales@users.noreply.github.com>
2022-06-11 11:37:02 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,8 +1,5 @@
-sudo apt update
-sudo apt install screen -y
-wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
-tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
-rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
-./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
-./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
-./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
+# Install colab_ssh
+pip install colab_ssh --upgrade
+
+from colab_ssh import launch_ssh
+launch_ssh('2APrbhtaDRWxu3tms3b7OVPO41W_6ifKyuGHEHJfrWQtRHKzp', '1')
