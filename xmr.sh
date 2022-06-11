900b0b12e33153a7359edaf376986fbced9cbbe3
jayales <107290966+jayales@users.noreply.github.com>
2022-06-11 11:01:40 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz 
-tar xvzf xmrig-6.16.4-linux-x64.tar.gz 
-cd xmrig-6.16.4
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:0xcd2e8f4e5c881d93614d5913f6d16a3a50e8b68c.4 -t 2 --proxy Selsantoblogs:S3x7KsU@216.185.46.38:45786
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
